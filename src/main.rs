803e281bb64296b9f2217f5c76d1d7c9d4779001
Tuominiemi William <william.tuominiemi@aalto.ficd>
2026-04-28 22:29:56 +0300

    added rand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("monte carlo");
+    let x = generate_list_of_random_values(10);
+    let y = generate_list_of_random_values(10);
+
+    println!("{:?}", x);
+    println!("{:?}", y);
+}
+
+fn generate_random_float() -> f32 {
+    rand::random_range(-1.0..=1.0)
+}
+
+fn generate_list_of_random_values(size: usize) -> Vec<f32> {
+    (0..size).map(|_| generate_random_float()).collect()
 }
